bf1946b9c03f684a738f7ed625b14df2a746aaf5
mgismissing <gabrycraft2010@gmail.com>
2026-08-10 22:50:52 +0200

    Add build and cleanup scripts

---
+++ build.sh
@@ -0,0 +1,12 @@
+cleanup.sh &&
+
+folder="src"
+args=()
+
+while IFS= read -r -d '' file; do
+    base="${file%.*}"
+    out="${base}.luau"
+    args+=("$file" "$out")
+done < <(find "$folder" -type f -name "*.z" -print0)
+
+z --lang lua --verbose --pretty "${args[@]}"
---
+++ cleanup.sh
@@ -0,0 +1,1 @@
+find src -type f -name "*.luau" ! -path "*/_*/*" ! -name "_*" -delete
